d14eda7ff25743bca263046b1e0e2e224041668e
Sma11oo28 <sma11oo.it@gmail.com>
2025-04-15 18:46:53 +0300

    Update particle.js

--- assets/js/particle.js
+++ assets/js/particle.js
@@ -20,12 +20,8 @@ particlesJS("particles-js", {
         },
         "polygon": {
           "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
         }
+        
       },
       "opacity": {
         "value": 0.3,
@@ -112,22 +108,4 @@ particlesJS("particles-js", {
   });
   
   
-  /* ---- stats.js config ---- */
-  
-  var count_particles, stats, update;
-  stats = new Stats;
-  stats.setMode(0);
-  stats.domElement.style.position = 'absolute';
-  stats.domElement.style.left = '0px';
-  stats.domElement.style.top = '0px';
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector('.js-count-particles');
-  update = function() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);
+ 
