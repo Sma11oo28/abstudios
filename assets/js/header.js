5df8bb078d04322deb48998282fc0f000f16fa11
Sma11oo28 <sma11oo.it@gmail.com>
2025-04-15 18:56:22 +0300

    Update header.js

--- assets/js/header.js
+++ assets/js/header.js
@@ -7,10 +7,15 @@ $(window).scroll(function () {
     if (!isScrolling) {
         requestAnimationFrame(() => {
             let scrollTop = $(this).scrollTop();
-            if (Math.abs(scrollTop - lastScrollTop) > delta) {
+
+            // Always show the header at the very top
+            if (scrollTop <= 0) {
+                header.css("top", "0");
+            } else if (Math.abs(scrollTop - lastScrollTop) > delta) {
                 header.css("top", scrollTop > lastScrollTop ? "-100px" : "0");
                 lastScrollTop = scrollTop;
             }
+
             isScrolling = false;
         });
         isScrolling = true;
